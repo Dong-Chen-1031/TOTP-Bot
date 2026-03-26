8a7e144b45e325a7a9cf8db353bbaf34bd2b6faf
Dong-Chen-1031 <dcdcdc1031@gmail.com>
2026-03-26 19:00:17 +0800

    Add OTP feature

---
+++ cogs/otp_cog.py
@@ -0,0 +1,60 @@
+import logging
+from datetime import datetime
+
+import discord
+import pyotp
+from discord import app_commands
+from discord.ext import commands
+
+from settings import OTP_KEY
+
+totp = pyotp.TOTP(OTP_KEY)
+
+
+class RefreshingBtn(discord.ui.Button):
+    def __init__(self):
+        super().__init__(
+            # emoji="",
+            label="刷新 OTP",
+            style=discord.ButtonStyle.primary,
+            custom_id="refresh_otp",
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+        otp = totp.now()
+        now = datetime.now().timestamp()
+        embed = discord.Embed(
+            title="Google 帳號 OTP",
+            description=f"你的 OTP 是：```{otp}```\n此密碼將在 <t:{int(now // totp.interval + 1) * totp.interval}:R> 過期",
+            color=discord.Color.blue(),
+        )
+        await interaction.response.edit_message(embed=embed)
+
+
+refreshing_btn = RefreshingBtn()
+view = discord.ui.View(timeout=None)
+view.add_item(refreshing_btn)
+
+
+class OTPCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(
+        name="otp", description="生成用於 Google 帳號登入的一次性密碼"
+    )
+    async def otp(self, interaction: discord.Interaction):
+        otp = totp.now()
+        now = datetime.now().timestamp()
+        embed = discord.Embed(
+            title="Google 帳號 OTP",
+            description=f"你的 OTP 是：```{otp}```\n此密碼將在 <t:{int(now // totp.interval + 1) * totp.interval}:R> 過期",
+            color=discord.Color.blue(),
+        )
+        await interaction.response.send_message(embed=embed, view=view)
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(OTPCog(bot))
+    bot.add_view(view)
+    logging.info(f"{__name__} 已載入")
--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 import os
 import sys
+
 from dotenv import load_dotenv
 
 # 載入 .env 檔案中的環境變數
@@ -10,19 +11,19 @@ load_dotenv()
 # Discord 機器人 Token
 DISCORD_BOT_TOKEN = str(os.getenv("DISCORD_BOT_TOKEN"))
 
-AUTO_RELOAD = False # 是否自動重新載入 Cogs
+AUTO_RELOAD = True  # 是否自動重新載入 Cogs
 
 # 開發者指令的開發者ID
-DEV_ID = [
-    123456789012345678,  # 替換為你的 Discord 用戶 ID
-    987654321098765432   # 可以添加多個開發者 ID
-]
+DEV_ID = [int(dev_id.strip()) for dev_id in os.getenv("DEV_ID").split(",")]
+
+
+OTP_KEY = os.getenv("OTP_KEY")
 
 # 檢查 Token 是否存在
 if DISCORD_BOT_TOKEN is None:
     print("錯誤: 找不到 DISCORD_BOT_TOKEN 環境變數")
     print("請確認 .env 檔案中已正確設置 DISCORD_BOT_TOKEN")
-    print("格式應為: DISCORD_BOT_TOKEN = \"你的Token\"")
+    print('格式應為: DISCORD_BOT_TOKEN = "你的Token"')
     sys.exit(1)
 
 # 機器人開發者指令前綴
